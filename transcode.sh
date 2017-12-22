e291552cb281af6cd0125eca08e3722142b37aa1
majamee <majamee@yandex.com>
2017-12-22 13:32:05 +0100

    Update transcode.sh

--- transcode.sh
+++ transcode.sh
@@ -48,7 +48,7 @@ rm "output/${filename}/intermed_1080p.mp4" "output/${filename}/intermed_720p.mp4
 # Add HTML code for easy inclusion in website
 echo -e "\nAdd HTML files for playback to output folder"
 cp /app/src/htaccess "output/${filename}/.htaccess"
-cp /app/src/player.html "output/${filename}/player.html"
+cp /app/src/plyr.html "output/${filename}/plyr.html"
 cp /app/src/index.html "output/${filename}/index.html"
 
 # Set permissions for newly created files and folders matching the video file's permissions
