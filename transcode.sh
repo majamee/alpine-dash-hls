723d4b91a87885f3faa93fe87f9b58cbfefbfa71
majamee <majamee@yandex.com>
2020-10-30 02:01:26 +0100

    Ensure output folder exists

--- transcode.sh
+++ transcode.sh
@@ -6,8 +6,8 @@ filename=$(basename "${input_file}")
 filename="${filename%.*}"
 frames=$(ffprobe -v error -select_streams v:0 -show_entries stream=nb_frames -of default=nokey=1:noprint_wrappers=1 "${input_file}")
 
-# make folders
 echo -e "\nCurrent video: ${input_file}\nDetected file name: ${filename}\nTotal # of frames: ${frames}";
+mkdir -p "output/${filename}/";
 
 if [[ -z "$2" ]]; then
   # Create Video Preview thumbnails
